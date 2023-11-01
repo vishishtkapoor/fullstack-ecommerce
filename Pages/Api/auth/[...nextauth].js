5523fd38a5177aad7ccf1032269125b0a1442b1b
vishishtkapoor <vishishtkapoor2@gmail.com>
2023-11-01 23:02:31 +0530

    mongoDB

--- Pages/Api/auth/[...nextauth].js
+++ Pages/Api/auth/[...nextauth].js
@@ -1,16 +1,30 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb"
 
 export const authOptions = {
-    // Configure one or more authentication providers
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
-        // ...add more providers here
-        
     ],
-}
+    pages: {
+        signIn: '/login',
+    },
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        session: async ({ token, session }) => {
+            if (session?.user && token?.sub) {
+                session.user.id = token.sub;
+            }
+            return session;
+        },
+    },
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
--- Pages/_app.js
+++ Pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
-import { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react";
+
+import en from 'javascript-time-ago/locale/en.json'
+
+TimeAgo.addDefaultLocale(en);
 
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+function MyApp({
+    Component,
+    pageProps: { session, ...pageProps },
+}) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
 
-export default MyApp;
+export default MyApp
